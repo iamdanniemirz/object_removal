86d61e0081c738b8679fc6f50dfbb8a900d31bd5
hypersoft Inc <danialmirza@hypersoft.com.pk>
2023-05-30 12:58:28 +0500

    app.py updated.

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
-# app.py
 from flask import Flask, flash, request, redirect, jsonify, send_file
 from werkzeug.utils import secure_filename
 import patch_match
 from PIL import Image
 import os
+import numpy as np
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'files/'
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def convert_image(image):
+    image = np.asarray(image)
+    image = image[:, :, :3]
+    out = Image.fromarray(image, 'RGB')
+    return out
+
+def convert_mask(image):
+    out = image.convert('L')
+    return out
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
     image = request.files['image']
     mask = request.files['mask']
+    ps = request.form['ps']
     if image.filename == '':
         flash('No image selected for uploading')
         return redirect(request.url)
@@ -35,12 +45,12 @@ def upload_image():
 
         image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name))
         mask.save(os.path.join(app.config['UPLOAD_FOLDER'], mask_name))
-        print('upload_image filename: ' + image_name)
-        print('upload_image filename: ' + mask_name)
 
         img = Image.open('files/' + image_name)
+        img = convert_image(img)
         msk = Image.open('files/' + mask_name)
-        result = patch_match.inpaint(img, msk, patch_size=1)
+        msk = convert_mask(msk)
+        result = patch_match.inpaint(img, msk, patch_size=int(ps))
         output = Image.fromarray(result, 'RGB')
         output.save('files/output.jpg')
 
@@ -48,11 +58,6 @@ def upload_image():
         os.remove('files/' + mask_name)
 
         return send_file(os.path.join('files', 'output.jpg'), mimetype='image/jpeg')
-
-
-# @app.route('/output', methods=['GET'])
-# def show_image():
-#     return send_file(os.path.join('static/uploads', 'output.jpg'), mimetype='image/jpeg')
 
 
 if __name__ == "__main__":
