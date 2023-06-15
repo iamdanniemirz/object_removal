3dee97d3cc6e145cafda99c6663b5b936ab8c535
Danial Mirza <danial@99technologies.co>
2023-06-16 03:09:22 +0500

    Files updated.

--- Dockerfile
+++ Dockerfile
@@ -63,7 +63,7 @@ COPY travis.sh .
 RUN make
 # RUN pip3 install -r requirements.txt
 #
-##EXPOSE 8000
-##CMD ["python", "app.py"]
+ENTRYPOINT ["python"]
+CMD ["app.py"]
 #
 #CMD ["app.handler"]
--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def handler(event, context):
 
 
 # if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8000)
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(debug=True, host='0.0.0.0', port=port)
